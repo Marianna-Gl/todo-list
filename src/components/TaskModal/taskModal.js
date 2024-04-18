cc11ecfb80bc0e0fec72ce3955978e3a25c341de
Marianna Galichyan <marianna.galichyan@gmail.com>
2024-04-19 01:20:50 +0400

    added NewTaskModal

---
+++ src/components/TaskModal/taskModal.js
@@ -0,0 +1,33 @@
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      title: '',
+      description: '',
+      dueDate: ''
+    }
+  },
+  methods: {
+    onClose() {
+      this.$emit('close')
+    },
+    onSave() {
+      const newTask = {
+        title: this.title,
+        description: this.description,
+        date: this.dueDate
+      }
+      this.$emit('taskSave', newTask)
+      this.onClose
+    },
+    onTitleInput(event) {
+      this.title = event.target.value
+    }
+  }
+}
